6302835fb02825fcc6f93c003e648a61d901726c
dangduomg <jack98765.2007@gmail.com>
2026-04-16 14:20:32 +0700

    add accessibility features (aria and stuff)

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -3,4 +3,7 @@ const shareButton = document.getElementById('share-btn');
 
 shareButton.addEventListener('click', () => {
     infoBar.classList.toggle('show-menu');
+    shareButton.setAttribute(
+        'aria-expanded', infoBar.classList.contains('show-menu')
+    );
 })
